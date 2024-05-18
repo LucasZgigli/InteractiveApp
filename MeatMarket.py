be7a38170e1990c464ea4071c9a582768657232f
LucasZgigli <161336572+LucasZgigli@users.noreply.github.com>
2024-05-18 15:12:15 +0100

    f

--- MeatMarket.py
+++ MeatMarket.py
@@ -36,7 +36,7 @@ df_selection = df.query('Year >= @year_range[0] and Year <= @year_range[1]  and 
 st.title('European Meat Market ')
 Country = df_selection['Area'].iloc[0]
 Population = int(df_selection['Population'].mean())*1000 # original unit is *1000 inhabitants
-Land = int((df_selection['Land area'].mean()*1000)) # same as land area multiplied by 1000 hectars
+Land = int(df_selection['Land area'].mean()*1000) # same as land area multiplied by 1000 hectars
 Pastures = int((df_selection['Permanent meadows and pastures'].mean()*1000)
 Total_export = int(df_selection['Export Quantity'].sum()*1000)
 Total_Production = int(df_selection['Production'].sum()*1000)
