5db080f463b3ecaa1f06358ddb6243fc5af73d58
LucasZgigli <161336572+LucasZgigli@users.noreply.github.com>
2024-05-19 01:05:39 +0100

    d

--- MeatMarket.py
+++ MeatMarket.py
@@ -247,7 +247,7 @@ st.plotly_chart(fig, use_container_width=True)
 # Define paths using raw string literals to avoid escape sequence issues
 # Define paths using raw string literals
 base_path = r'C:\Users\lucas\OneDrive\Área de Trabalho\CCT\Github\InteractiveApp'
-model_path = os.path.join(base_path, 'NNmodel_resaved.h5')
+model_path = os.path.join('C:\Users\lucas\OneDrive\Área de Trabalho\CCT\Github\InteractiveApp\NNmodel.h5')
 scaler_path = os.path.join(base_path, 'scaler.pkl')
 item_encoder_path = os.path.join(base_path, 'Item_encoder.pkl')
 area_encoder_path = os.path.join(base_path, 'Area_encoder.pkl')
