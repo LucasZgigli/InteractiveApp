faa1924a34beaab8fe71cd714332a805e2d45559
LucasZgigli <161336572+LucasZgigli@users.noreply.github.com>
2024-05-19 23:23:16 +0100

    Update MeatMarket.py

--- MeatMarket.py
+++ MeatMarket.py
@@ -30,6 +30,7 @@ df = pd.read_csv('Meat_market.csv')
 df['Item'].unique()
 
 # Set up page
+# Set up the Streamlit page
 st.set_page_config(page_title='Meat Market', page_icon=":flag-eu:", layout="wide")
 
 # Configure the sidebar for user input
@@ -43,118 +44,117 @@ year_range = st.sidebar.slider(
 
 selected_area = st.sidebar.selectbox('Select Area', options=df['Area'].unique())
 
-# Filter the DataFrame based on user input
+metric = st.sidebar.selectbox(
+    'Select Metric',
+    options=['Production', 'Export Quantity', 'Domestic supply quantity']
+)
 
+# Filter the DataFrame based on user input
 df_selection = df.query('Year >= @year_range[0] and Year <= @year_range[1]  and Area == @selected_area')
 
-#Main page
-st.title('European Meat Market ')
+# Main page
+st.title('European Meat Market')
 df_selection_sorted = df_selection.sort_values(by='Year', ascending=False)
 Country = df_selection['Area'].iloc[0]
-Population = int(df_selection_sorted['Population'].iloc[0])*1000 # original unit is *1000 inhabitants
-Land = int(df_selection_sorted['Country area'].iloc[0]*1000) # same as land area multiplied by 1000 hectars
-Pastures = int(df_selection_sorted['Permanent meadows and pastures'].iloc[0]*1000)
-Total_export = int(df_selection['Export Quantity'].sum()*1000)
-Total_Production = int(df_selection['Production'].sum()*1000)
-Total_SupplyQuantity = int(df_selection['Domestic supply quantity'].sum()*1000)
-#Calling the most recent GDP )
+Population = int(df_selection_sorted['Population'].iloc[0]) * 1000  # original unit is *1000 inhabitants
+Land = int(df_selection_sorted['Country area'].iloc[0] * 1000)  # same as land area multiplied by 1000 hectares
+Pastures = int(df_selection_sorted['Permanent meadows and pastures'].iloc[0] * 1000)
+Total_export = int(df_selection['Export Quantity'].sum() * 1000)
+Total_Production = int(df_selection['Production'].sum() * 1000)
+Total_SupplyQuantity = int(df_selection['Domestic supply quantity'].sum() * 1000)
+# Calling the most recent GDP
 most_recent_gdp_per_capita = int(df_selection_sorted['GDP per capita in USD'].iloc[0])
 GDP = most_recent_gdp_per_capita
-          
-# Placing a markdown   
+
+# Placing a markdown
 st.markdown("""---""")
-#Splitting the header into 3 columns
+# Splitting the header into 3 columns
 left_column, middle_column, right_column = st.columns(3)
 with left_column:
-    
     st.header(f'{Country}')
     st.subheader(f'Population: {Population:,} inh')
-    st.subheader(f'Land Area: {Land:,}Ha')
-                       
+    st.subheader(f'Land Area: {Land:,} Ha')
+
 with middle_column:
     st.header(f'')
-    st.subheader(f'Total Production: {Total_Production:,}t') 
-    st.subheader(f'Total Export Quantity: {Total_export:,}t')
+    st.subheader(f'Total Production: {Total_Production:,} t')
+    st.subheader(f'Total Export Quantity: {Total_export:,} t')
 
-    
 with right_column:
     st.header(f'')
-    st.subheader(f'Total Supply Quantity: {Total_SupplyQuantity:,}t')
-    st.subheader(f'GDP: {GDP:,} in Million USD') 
-    
-# Placing a markdown   
+    st.subheader(f'Total Supply Quantity: {Total_SupplyQuantity:,} t')
+    st.subheader(f'GDP: {GDP:,} in Million USD')
+
+# Placing a markdown
 st.markdown("""---""")
 
-#creating 2 columns                       
-col1, col2, col3 = st.columns(3)   
+# Creating 2 columns
+col1, col2, col3 = st.columns(3)
 
-# Grouping by item and the sum of production 
-production_by_item = df_selection.groupby('Item')['Production'].sum().sort_values(ascending=False)*1000 #converting the unit 
+# Grouping by item and the sum of production
+production_by_item = df_selection.groupby('Item')['Production'].sum().sort_values(ascending=False) * 1000  # converting the unit
 
 # Plotting using Plotly Express
-#Production in the first column
-with col1: 
+# Production in the first column
+with col1:
     Prod_ = px.bar(
         production_by_item,
         x=production_by_item.values,
         y=production_by_item.index,
         orientation='h',
         title="<b>Production by Item</b>",
-        color_discrete_sequence=["#0083B8"],  
+        color_discrete_sequence=["#0083B8"],
         template="plotly_white"
     )
 
     Prod_.update_layout(
         plot_bgcolor="rgba(0,0,0,0)",
-        xaxis=dict(showgrid=False, title='Total Production in t' )
+        xaxis=dict(showgrid=False, title='Total Production in t')
     )
-# plotting
     st.plotly_chart(Prod_)
-                       
-#Supply Quantity in the second column
-supplyquantity_by_item = df_selection.groupby('Item')['Domestic supply quantity'].sum().sort_values(ascending=False)*1000 #converting the unit   
-                 
-with col2: 
-    Prod_ = px.bar(
-        production_by_item,
+
+# Supply Quantity in the second column
+supplyquantity_by_item = df_selection.groupby('Item')['Domestic supply quantity'].sum().sort_values(ascending=False) * 1000  # converting the unit
+
+with col2:
+    Supply_ = px.bar(
+        supplyquantity_by_item,
         x=supplyquantity_by_item.values,
         y=supplyquantity_by_item.index,
         orientation='h',
         title="<b>Supply Quantity by Item</b>",
-        color_discrete_sequence=["#0083B8"],  
+        color_discrete_sequence=["#0083B8"],
         template="plotly_white"
     )
 
-    Prod_.update_layout(
+    Supply_.update_layout(
         plot_bgcolor="rgba(0,0,0,0)",
-        xaxis=dict(showgrid=False, title='Total Domestic Supply Quantity in t' )
+        xaxis=dict(showgrid=False, title='Total Domestic Supply Quantity in t')
     )
-# plotting
-    st.plotly_chart(Prod_)     
-                 
-#Export Quantity in the second column                       
-export_by_item = df_selection.groupby('Item')['Export Quantity'].sum().sort_values(ascending=False)*1000
+    st.plotly_chart(Supply_)
 
-with col3:                       
-    exp_ = px.bar(
+# Export Quantity in the second column
+export_by_item = df_selection.groupby('Item')['Export Quantity'].sum().sort_values(ascending=False) * 1000
+
+with col3:
+    Exp_ = px.bar(
         export_by_item,
         x=export_by_item.values,
         y=export_by_item.index,
         orientation='h',
         labels={'x': 'Total Quantity Exported in Tonnes', 'index': 'Item'},
         title="<b>Export by Item</b>",
-        color_discrete_sequence=["#0083B8"], 
+        color_discrete_sequence=["#0083B8"],
         template="plotly_white"
     )
 
-    Prod_.update_layout(
+    Exp_.update_layout(
         plot_bgcolor="rgba(0,0,0,0)",
-        xaxis=dict(showgrid=False, title='Export Quantity in t' )
+        xaxis=dict(showgrid=False, title='Export Quantity in t')
     )
-# plotting
-    st.plotly_chart(exp_)   
-    
-# placing another markdown    
+    st.plotly_chart(Exp_)
+
+# Placing another markdown
 st.markdown("""---""")
 
 # Group by Year and Item
@@ -164,94 +164,37 @@ yearly_data = df_selection.groupby(['Year', 'Item']).agg({
     'Domestic supply quantity': 'sum'
 }).reset_index()
 
-
 yearly_data['Production'] *= 1000
 yearly_data['Export Quantity'] *= 1000
 yearly_data['Domestic supply quantity'] *= 1000
 
-
 # Sorting by Year
 yearly_data.sort_values('Year', inplace=True)
 
-# Create initial traces for Production
+# Create initial traces for the selected metric
 fig = go.Figure()
 
-
-# Production
+# Add traces for the selected metric
 for item in yearly_data['Item'].unique():
     item_data = yearly_data[yearly_data['Item'] == item]
     fig.add_trace(go.Scatter(
         x=item_data['Year'],
-        y=item_data['Production'],
+        y=item_data[metric],
         mode='lines',
-        name=f'{item} - Production'
-    ))
-
-# Export Quantity
-for item in yearly_data['Item'].unique():
-    item_data = yearly_data[yearly_data['Item'] == item]
-    fig.add_trace(go.Scatter(
-        x=item_data['Year'],
-        y=item_data['Export Quantity'],
-        mode='lines',
-        name=f'{item} - Export Quantity',
-        visible=False 
-    ))
-
-# Domestic supply quantity
-for item in yearly_data['Item'].unique():
-    item_data = yearly_data[yearly_data['Item'] == item]
-    fig.add_trace(go.Scatter(
-        x=item_data['Year'],
-        y=item_data['Domestic supply quantity'],
-        mode='lines',
-        name=f'{item} - Domestic supply quantity',
-        visible=False  
+        name=f'{item} - {metric}'
     ))
 
 # Update layout
 fig.update_layout(
-    title='Annual Sum of Production, Export Quantities, and Domestic Supply Quantity by Meat Type',
+    title=f'Annual Sum of {metric} by Meat Type',
     xaxis_title='Year',
     yaxis_title='Total Quantity',
     legend_title='Metrics',
-    plot_bgcolor='rgba(0,0,0,0)',
-    updatemenus=[{
-        'buttons': [
-            {
-                'method': 'update',
-                'label': 'Production',
-                'args': [
-                    {'visible': [i < len(yearly_data['Item'].unique()) for i in range(len(fig.data))]},
-                    {'title': 'Production Quantities by Meat Type'}
-                ]
-            },
-            {
-                'method': 'update',
-                'label': 'Export Quantity',
-                'args': [
-                    {'visible': [len(yearly_data['Item'].unique()) <= i < 2 * len(yearly_data['Item'].unique()) for i in range(len(fig.data))]},
-                    {'title': 'Export Quantities by Meat Type'}
-                ]
-            },
-            {
-                'method': 'update',
-                'label': 'Domestic supply quantity',
-                'args': [
-                    {'visible': [2 * len(yearly_data['Item'].unique()) <= i < 3 * len(yearly_data['Item'].unique()) for i in range(len(fig.data))]},
-                    {'title': 'Domestic Supply Quantities by Meat Type'}
-                ]
-            }
-        ],
-        'direction': 'down',
-        'showactive': True,
-    }]
+    plot_bgcolor='rgba(0,0,0,0)'
 )
 
 # Plotting
 st.plotly_chart(fig, use_container_width=True)
-
-
 # Define paths using raw string literals
 # Define relative paths
 model_path = 'ANNmodel.h5'
